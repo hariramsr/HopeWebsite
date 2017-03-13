894714fd1e90288438366bc8416d0759e99fbfa4
HariRam <hariramsr08@gmail.com>
2017-03-13 12:00:30 +0530

    Changes made to home carousel with shared images inheader and added logo

--- app/Home/homeController.js
+++ app/Home/homeController.js
@@ -1,45 +1,13 @@
+  
 'use strict';
 
-angular.module('hopeApp')    
+angular.module('hopeApp')
+    
+    .controller('homeController', ['$scope', 'hopeService', function ($scope, hopeService) {
+    var vm = this;
+    var resources = hopeService.getDataFromJSON();
+                console.log(resources.about);
 
-.controller('homeController', ['$scope', 'menuFactory', 'corporateFactory', function ($scope, menuFactory, corporateFactory) {
 
-        $scope.showDish = false;
-        $scope.showPromotion = false;
-        $scope.showLeader = false;
-        $scope.message = "Loading ...";
 
-        $scope.featuredDish = menuFactory.getDishes().get({id: 0})
-            .$promise.then(
-                function (response) {
-                    $scope.featuredDish = response;
-                    $scope.showDish = true;
-                },
-                function (response) {
-                    $scope.message = "Error: " + response.status + " " + response.statusText;
-                }
-            );
-
-        $scope.featuredPromotion = menuFactory.getPromotions().get({id: 0})
-            .$promise.then(
-                function (response) {
-                    $scope.featuredPromotion = response;
-                    $scope.showPromotion = true;
-                },
-                function (response) {
-                    $scope.message = "Error: " + response.status + " " + response.statusText;
-                }
-            );
-
-        $scope.executiveChef = corporateFactory.getLeaders().get({id: 3})
-            .$promise.then(
-                function (response) {
-                    $scope.executiveChef = response;
-                    $scope.showLeader = true;
-                },
-                function (response) {
-                    $scope.message = "Error: " + response.status + " " + response.statusText;
-                }
-            );
-
-    }]);
+}]);
